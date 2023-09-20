f5886f11217474df449b00b789e020cc016b31ec
Travis Mix <travismix1980@gmail.com>
2023-09-19 17:02:30 -0700

    App Complete

---
+++ Pages/OrderScreen.razor.cs
@@ -0,0 +1,42 @@
+﻿namespace CalculatorApp.Pages
+{
+    public partial class OrderScreen
+    {
+        private double craftsman = 67.45;
+        private double neverkink = 99.99;
+        private double steel = 85.67;
+        private double ultralite = 47.92;
+        public string? TotalPrice { get; set; } = "$0.00";
+        public double CraftQty { get; set; } = 0;
+        public double KinkQty { get; set; } = 0;
+        public double SteelQty { get; set; } = 0;
+        public double UltraQty { get; set; } = 0;
+        public double CraftSubTotal { get; set; } = 0;
+        public double KinkSubTotal { get; set; } = 0;
+        public double SteelSubTotal { get; set; } = 0;
+        public double UltraSubTotal { get; set; } = 0;
+
+        private void CalculateTotalPrice()
+        {
+            CraftSubTotal = CraftQty * craftsman;
+            KinkSubTotal = KinkQty * neverkink;
+            SteelSubTotal = SteelQty * steel;
+            UltraSubTotal = UltraQty * ultralite;
+            double temp = CraftSubTotal + KinkSubTotal + SteelSubTotal + UltraSubTotal;
+            TotalPrice = $"${temp.ToString("N2")}";
+        }
+
+        private void NewOrder()
+        {
+            CraftQty = 0;
+            KinkQty = 0;
+            SteelQty = 0;
+            UltraQty = 0;
+            CraftSubTotal = 0;
+            KinkSubTotal = 0;
+            SteelSubTotal = 0;
+            UltraSubTotal = 0;
+            TotalPrice = "$0.00";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿* Calculator and Order sheet page made in HTML / Bootstrap / C# / Blazor
+- Built a simple calculator where you enter in your two numbers and then click the button for the thing you want to do
+- Built a simple hose ordering page
+-- Select the number of each type of hose you want and press the total button to see the subtotals and total price
+-- Click the new order button to reset the page
